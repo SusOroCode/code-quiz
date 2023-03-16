8d208d0c67d5c49533aa71e8bc3fd024105adc7e
Tanuki <susann.orosco33@gmail.com>
2023-03-15 19:21:24 -0700

    added variables to javascript, worked on HTML and CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 // Timer Coundown https://codepen.io/tutsplus/pen/WNzqPaL
+
+//Variables
+
+var highScores = document.querySelector("#viewhighscores"); //Top Paragraph link showing scores when clicked
+var countDown = document.querySelector("#countdown"); //Top right timer showing time left
+var startButton = document.querySelector("#start-btn"); // First page start button to click to start game
+var titleQuestions = document.querySelector("#titleandquestions"); //Used for quiz title and all questions in following pages
